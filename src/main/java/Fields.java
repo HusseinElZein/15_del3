4a0531cf1ee7867c7ee35f4c857ec675dad1de6d
Hussein <s211904@dtu.dk>
2021-11-15 19:12:38 +0100

    Created classes Fields and the singleton class GUI_controller

---
+++ src/main/java/Fields.java
@@ -0,0 +1,34 @@
+import gui_fields.*;
+
+public class Fields {
+    GUI_Field[] fields = new GUI_Field[24];
+
+
+    public void CreateFields() {
+        for (int i = 0; i <= 23; i++) {
+            fields[i] = new GUI_Street();
+        }
+        jailField();
+    }
+
+    public GUI_Field[] getFields(){
+        return fields;
+    }
+
+    public GUI_Field getSpecificField(int specificField){
+        return fields[specificField];
+    }
+
+    public void jailField(){
+        fields[6] = new GUI_Jail();
+        fields[18] = new GUI_Jail();
+    }
+
+    public void ownField(){
+
+
+
+    }
+
+
+}
---
+++ src/main/java/GUI_controller.java
@@ -0,0 +1,35 @@
+import gui_fields.GUI_Field;
+import gui_main.GUI;
+
+class GUI_controller {
+    private static GUI board;
+    private Fields fields = new Fields();
+    private static GUI GUI_instance = null;
+
+    private GUI GUI_controller() {
+        fields.CreateFields();
+        board = new GUI(fields.getFields());
+        return board;
+    }
+
+    public GUI getInstance() {
+        if (GUI_instance == null) {
+            GUI_controller k = new GUI_controller();
+            GUI_instance = k.GUI_controller();
+        }
+        return GUI_instance;
+    }
+
+    public GUI_Field getSpecificField(int specificFields) {
+        GUI_Field spec = fields.getSpecificField(specificFields);
+        return spec;
+    }
+
+}
+
+
+
+
+
+
+
