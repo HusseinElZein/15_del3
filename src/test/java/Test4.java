6319c715ea9e687328070de17d5531d8c4caf40d
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2021-11-24 22:57:34 +0100

    Now the tests are in real order

--- src/test/java/Test4.java
+++ src/test/java/Test4.java
@@ -1,71 +1,38 @@
 import gui_fields.*;
 import gui_main.GUI;
-
 import java.awt.*;
 
 public class Test4 {
 
-    public static void main(String[] args) {
+    //This test is showing us how we can initialize one player, and move on a board
+    public void myFields(){
         GUI_Field[] fields = new GUI_Field[24];
         GUI board;
-        Dice dice = new Dice();
 
         for(int i=0; i<=23; i++) {
             fields[i] = new GUI_Street();
         }
-
-        GUI_Street testStreet = new GUI_Street();
-        testStreet.setTitle("høje gladsaxe");
-        testStreet.setSubText("Pris: 3");
-        testStreet.setBackGroundColor(Color.green);
-        testStreet.setRent("-3");
-        fields[1] = testStreet;
-
-        System.out.println();
-
         board = new GUI(fields);
 
-        String option = board.getUserSelection("Hvor mange spillere?", "2", "3", "4");
-        int chosenOption = Integer.parseInt(option);
+        GUI_Car car = new GUI_Car(Color.red,Color.white, GUI_Car.Type.RACECAR, GUI_Car.Pattern.CHECKERED);
+        GUI_Player player = new GUI_Player("Yusuf", 1000, car);
+        board.addPlayer(player);
+        Dice dice = new  Dice();
+        Player playerOurs = new Player();
 
-        Player[] ourPlayers = new Player[chosenOption];
 
-        GUI_Car[] cars = new GUI_Car[4];
-        cars[0] = new GUI_Car();
-        cars[0].setPrimaryColor(Color.green);
-        cars[1] = new GUI_Car();
-        cars[1].setPrimaryColor(Color.black);
-        cars[2] = new GUI_Car();
-        cars[2].setPrimaryColor(Color.blue);
-        cars[3] = new GUI_Car();
-        cars[3].setPrimaryColor(Color.red);
-
-        //creating number of players of what is chosen by the user from the GUI
-        GUI_Player[] players = new GUI_Player[chosenOption];
-
-        for(int i=0; i<chosenOption; i++){
-            String spillernavn = board.getUserString("Spiller " + (i+1) +", Indtast et navn");
-            ourPlayers[i] = new Player();
-            players[i] = new GUI_Player(spillernavn,ourPlayers[i].getMoney(),cars[i]);
-            board.addPlayer(players[i]);
-            fields[0].setCar(players[i], true);
-        }
-
-        int i = 0;
-        while(true){
-            board.getUserButtonPressed(players[i].getName() +", tryk for at kaste", "kast");
-            fields[ourPlayers[i].getSquare()].setCar(players[i], false);
+        while(true) {
+            board.getUserButtonPressed("tryk for at kaste", "kast");
+            fields[playerOurs.getSquare()].setCar(player, false);
             dice.throwDice();
             board.setDice(dice.getDie1(), dice.getDie2());
-
-            ourPlayers[i].moveToSquare(dice.getDie1(), dice.getDie2());
-
-            fields[ourPlayers[i].getSquare()].setCar(players[i], true);
-
-            i++;
-            i %= chosenOption;
+            fields[playerOurs.moveToSquare(dice.getDie1(), dice.getDie2())].setCar(player, true);
         }
     }
 
+    public static void main(String[] args) {
+        Test4 intoGuiTest = new Test4();
+        intoGuiTest.myFields();
+    }
 
 }
