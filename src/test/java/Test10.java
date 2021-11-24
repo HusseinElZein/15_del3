9e0e25db65bfcf0fbb3e4de6de2718e90a953d22
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2021-11-24 23:09:08 +0100

    Deleted some tests that weren't really tests

--- src/test/java/Test10.java
+++ src/test/java/Test10.java
@@ -1,54 +1,88 @@
-import gui_fields.GUI_Car;
-import gui_fields.GUI_Player;
-
-import java.awt.*;
+//this unit (TDD) test is testing a method inside Game. It is to ensure that
+//the system checks for the richest player in order to win.
+//Also in this test I'm (Hussein) trying to figure out how to make it work,
+//so that afterwards I can implement it.
+//And also the dilemma: What if there are two richest players?
+//That will mean that it is a draw between those two,
+//I know it is a stupid rule, but it is a junior, and juniors don't
+//like to stay out after going bankrupt for too long
 
 public class Test10 {
 
-    //This test is to ensure that the transfer works
-    //between an owner of a field and the player who lands on it
+    private static Player[] ourPlayers = new Player[4];
+    private boolean gameEnded;
+    private boolean draw;
+    private Player winner2;
 
+    public void checkForLoser(Player ourPlayer){
+
+        ourPlayer.setMoney(-5);
+
+
+        if(ourPlayer.getMoney() <= 0) {
+            System.out.println(ourPlayer.getName() + " er gaaet bankerot");
+            gameEnded = true;
+        }
+    }
+
+    public void checkForWinner(Player player){
+
+        Player winner = player;
+
+        for(int i=0; i<ourPlayers.length; i++){
+
+            if(ourPlayers[i].getMoney() > winner.getMoney()){
+                winner = ourPlayers[i];
+            }
+        }
+        checkIfDraw(winner);
+        if(draw != true) {
+            System.out.println(winner.getName() + " har vundet spillet! Hurra!");
+        }else if (draw == true){
+            System.out.println("Spillet er uafgjort mellem " + winner.getName() + " og " + winner2.getName());
+        }
+    }
+
+
+    public Player checkIfDraw(Player winner){
+
+        for(int i=0; i<ourPlayers.length; i++){
+
+            if(winner.getMoney() == ourPlayers[i].getMoney()){
+                if(winner != ourPlayers[i]) {
+                    winner2 = ourPlayers[i];
+                    draw = true;
+                }
+            }
+        }
+        return winner2;
+    }
+
+    public void findWinnerIfLoser(Player player) {
+        checkForLoser(player);
+        if (gameEnded == true) {
+            checkForWinner(player);
+        }
+    }
 
     public static void main(String[] args) {
+        Test10 test12 = new Test10();
 
-        Player receiver = new Player();
-        Player payer = new Player();
-        GameBoard fields = new GameBoard();
-        fields.createFields();
+        //ourPlayers = new Player[2];
+        ourPlayers[0] = new Player();
+        ourPlayers[1] = new Player();
+        ourPlayers[2] = new Player();
+        ourPlayers[3] = new Player();
 
-        GUI_Car car = new GUI_Car();
-        car.setPrimaryColor(Color.black);
+        ourPlayers[0].setName("player0");
+        ourPlayers[1].setName("player1");
+        ourPlayers[2].setName("player2");
+        ourPlayers[3].setName("player3");
 
-        GUI_Player players = new GUI_Player("Name",receiver.getMoney(),car);
+        ourPlayers[1].setMoney(-3);
+        ourPlayers[2].setMoney(-3);
+        ourPlayers[3].setMoney(-2);
 
-        receiver.moveToHere(1);
-        //fields.buyField(receiver,players,car);
-
-        fields.setOwnerOfField(receiver.getSquare(),receiver, players);
-
-
-        payer.moveToHere(1);
-
-
-        payer.transferMoney((-fields.getFieldRent(payer.getSquare())),
-                fields.getOwnerOfField(payer.getSquare()), payer);
-
-        payer.transferMoney((-fields.getFieldRent(payer.getSquare())),
-                fields.getOwnerOfField(payer.getSquare()), payer);
-
-        payer.transferMoney((-fields.getFieldRent(payer.getSquare())),
-                fields.getOwnerOfField(payer.getSquare()), payer);
-
-
-
-        System.out.println(fields.getOwnerOfField(payer.getSquare()).getMoney());
-
-        System.out.println(payer.getMoney());
-
-
-
-        fields.getOwnerOfField(1).getMoney();
-
-        fields.buyField(receiver,players,car);
+        test12.findWinnerIfLoser(ourPlayers[0]);
     }
 }
--- src/test/java/Test8.java
+++ src/test/java/Test8.java
@@ -1,26 +1,54 @@
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Player;
+
+import java.awt.*;
+
 public class Test8 {
 
-    //This test is to check if the player turns over the go field to
-    //recieve money
+    //This test is to ensure that the transfer works
+    //between an owner of a field and the player who lands on it
+
+
     public static void main(String[] args) {
-        Player player = new Player();
 
-        player.moveToSquare(13, 12);
-        System.out.println("Player's money right now: " + player.getMoney());
-        System.out.println("Player has overgone Go Field: " + player.checkOverGo());
+        Player receiver = new Player();
+        Player payer = new Player();
+        GameBoard fields = new GameBoard();
+        fields.createFields();
 
-        if(player.checkOverGo()==true){
-            player.setMoney(2);
-            player.setOverGoFalse();
-        }
-        System.out.println("Player's money right now: " + player.getMoney());
+        GUI_Car car = new GUI_Car();
+        car.setPrimaryColor(Color.black);
+
+        GUI_Player players = new GUI_Player("Name",receiver.getMoney(),car);
+
+        receiver.moveToHere(1);
+        //fields.buyField(receiver,players,car);
+
+        fields.setOwnerOfField(receiver.getSquare(),receiver, players);
 
 
-        if(player.checkOverGo()==true){
-            player.setMoney(2);
-            player.setOverGoFalse();
-        }
-        System.out.println("Player has overgone Go Field: " + player.checkOverGo());
-        System.out.println("Player's money right now: " + player.getMoney());
+        payer.moveToHere(1);
+
+
+        payer.transferMoney((-fields.getFieldRent(payer.getSquare())),
+                fields.getOwnerOfField(payer.getSquare()), payer);
+
+        payer.transferMoney((-fields.getFieldRent(payer.getSquare())),
+                fields.getOwnerOfField(payer.getSquare()), payer);
+
+        payer.transferMoney((-fields.getFieldRent(payer.getSquare())),
+                fields.getOwnerOfField(payer.getSquare()), payer);
+
+
+
+        System.out.println(fields.getOwnerOfField(payer.getSquare()).getMoney());
+
+        System.out.println(payer.getMoney());
+
+
+
+        fields.getOwnerOfField(1).getMoney();
+
+        fields.buyField(receiver,players,car);
     }
 }
