936ef3f889ec810d6ea7bb92f7d12f9025afc5aa
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2021-11-24 22:46:15 +0100

    Test13 is now complete and now player's cannot be called the same whatsoever

---
+++ src/test/java/Test13.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+//This is a TDD test, in which I (Hussein) want to figure out how to
+//prevent players from naming themselves the same name.
+
+public class Test13 {
+    private static Player[] players = new Player[4];
+    private int numberOfPlayers = 4;
+    private boolean same;
+
+    private boolean playerNamesAreSame(String name, int playerNumberYet) {
+
+        for (int i = 0; i < playerNumberYet; i++) {
+            if (!name.equals(players[i].getName())){
+                same = false;
+            }
+        }
+        for (int i = 0; i < playerNumberYet; i++) {
+            if (name.equals(players[i].getName())) {
+                same = true;
+            }
+        }
+        return same;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Test13 test = new Test13();
+
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Navn til spiller " + (i + 1) + ": ");
+            String nameOfPlayer = sc.next();
+
+            players[i] = new Player();
+            players[i].setName(nameOfPlayer);
+
+            if (i > 0) {
+
+                while(test.playerNamesAreSame(nameOfPlayer, i) == true) {
+                    System.out.print("Spiller " + (i+1) + ", navnet er allerede taget, skriv et nyt: ");
+                    nameOfPlayer = sc.next();
+                }
+                players[i].setName(nameOfPlayer);
+            }
+        }
+
+            for (int i = 0; i < 4; i++) {
+                System.out.println("Spiller " + (i + 1) + ": " + players[i].getName());
+            }
+    }
+}
+
